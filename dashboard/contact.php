dc066d764342fcea0fb6c3d5d1d537c28551549c
Gaurab-sunar <gaurabsuna@gmail.com>
2023-12-02 13:20:51 +0545

    gaurab

--- dashboard/contact.php
+++ dashboard/contact.php
@@ -26,6 +26,26 @@ if (isset($_GET['delete'])) {
   }
 }
 
+$userName = "";
+$userEmail = "";
+$userTitle = "";
+$userDes = "";
+
+
+if (isset($_GET["view"])) {
+  $viewId = $_GET["view"];
+  $sqlOne = "SELECT * FROM contact WHERE id='$viewId'";
+  $resultOne = $conn->query($sqlOne);
+  if ($resultOne->num_rows > 0) {
+    $row = $resultOne->fetch_assoc();
+    $userName = $row['name'];
+    $userEmail = $row['email'];
+    $userTitle = $row['title'];
+    $userDes = $row['description'];
+  }
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -37,6 +57,34 @@ if (isset($_GET['delete'])) {
   <title>Sajha Subidha</title>
   <link rel="stylesheet" href="./styles/style.css" />
   <link rel="stylesheet" href="./styles/subcategorys.css" />
+  <style>
+    #contentContact {
+      padding: 10px;
+      display: flex;
+      gap: 6px;
+      flex-direction: column;
+    }
+
+    #contentContact div {
+      box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.2);
+      padding: 7px;
+      border-radius: 3px;
+      font-family: Arial, Helvetica, sans-serif;
+    }
+
+    #modalContentLayer {
+      height: 100%;
+      min-height: 275px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 500px;
+    }
+
+    #Modal {
+      height: unset;
+    }
+  </style>
 </head>
 
 <body>
@@ -78,7 +126,7 @@ if (isset($_GET['delete'])) {
               <th>Email</th>
               <th>Title</th>
               <th>Description</th>
-              <th>Action</th>
+              <th colspan="2">Action</th>
             </tr>
           </thead>
           <tbody>
@@ -101,6 +149,7 @@ if (isset($_GET['delete'])) {
                 <td>' . $row['email'] . '</td>
                 <td>' . $limitedTitle . '</td>
             <td>' . $limitedDescription . '</td>
+            <td><a href="?view=' . $row['id'] . '">' . file_get_contents("./svg/eye.svg") . '</a></td>
             <td><a href="?delete=' . $row['id'] . '">' . file_get_contents("./svg/delete.svg") . '</a></td>
         </tr>';
               }
@@ -112,6 +161,66 @@ if (isset($_GET['delete'])) {
         </table>
       </div>
     </div>
+
+    <!-- ===================== for modal ================== -->
+    <div id="modalHiddenBlock">
+      <div id="modalOuterLayer">
+        <div id="modalContentLayer">
+          <div id="Modal">
+            <div id="modalHead">
+              <p style="text-transform: capitalize;">Message From
+                <?php echo $userName ?>
+              </p>
+              <div onclick="closeModal()">X</div>
+            </div>
+            <hr />
+            <div id="contentContact">
+              <div>Name:
+                <?php echo $userName ?>
+              </div>
+              <div>Email:
+                <?php echo $userEmail ?>
+              </div>
+              <div>Title:
+                <?php echo $userTitle ?>
+              </div>
+              <div>Description:
+                <?php echo $userDes ?>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script>
+      const modalHiddenBlock = document.getElementById("modalHiddenBlock");
+
+      const ModalOpen = () => {
+        if (modalHiddenBlock.style.display === "" || modalHiddenBlock.style.display === "none") {
+          modalHiddenBlock.style.display = "block";
+        } else {
+          modalHiddenBlock.style.display = "none";
+        }
+      };
+
+      const closeModal = () => {
+        modalHiddenBlock.style.display = "none";
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.delete('view');
+
+        const newUrl = window.location.pathname + '?' + urlParams.toString();
+        history.replaceState({}, document.title, newUrl);
+        location.reload();
+      };
+
+      const urlParams = new URLSearchParams(window.location.search);
+      const viewParam = urlParams.get('view');
+
+      if (viewParam) {
+        ModalOpen();
+      }
+
+    </script>
   </div>
 </body>
 
